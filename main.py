07cf6955ee5563017e9efc1da92ccd3ed01955b0
pzp <pzpzpa>
2021-02-04 18:12:30 +0200

    Code review modifications.

--- main.py
+++ main.py
@@ -10,14 +10,17 @@ class TicTacToe:
     def __init__(self, size=BOARD_SIZE):
         """Initialise the board, and set the starting player as 'X'."""
         self.board = [[EMPTY_SQUARE for _ in range(size)] for __ in range(size)]
-        self.player = PIECE_ONE
+        self.active_player = PIECE_ONE
+
+    def __call__(self):
+        self.game_loop()
 
     def __str__(self):
         output_str = " " * 4
-        for index in range(len(self.board[0])):
+        for index in range(BOARD_SIZE):
             output_str += str(index + 1) + (" " * 3)
         output_str += "\n"
-        for row_index in range(len(self.board)):
+        for row_index in range(BOARD_SIZE):
             output_str += string.ascii_uppercase[row_index]
             for cell in self.board[row_index]:
                 output_str += " | " + cell
@@ -25,19 +28,18 @@ class TicTacToe:
         return output_str
 
     def change_player(self):
-        player = PIECE_TWO if self.player == PIECE_ONE else PIECE_ONE
-        return player
+        self.active_player = PIECE_TWO if self.active_player == PIECE_ONE else PIECE_ONE
 
     def player_input(self):
         while True:
-            input_value = input("Please select a row and a column to place the" + self.player + "!").upper()
+            input_value = input("Please select a row and a column to place the " + self.active_player + "!").upper()
             if len(input_value) != 2:  # Input needs to be 2 characters long
                 continue
             else:
-                row_index = string.ascii_uppercase.find(input_value[0])  # Transform the letter into an index
-                col_index = int(input_value[1]) - 1  # Transform human input to index
                 if input_value[0] not in string.ascii_uppercase or not input_value[1].isdigit():  # if value not letter
                     continue
+                row_index = string.ascii_uppercase.find(input_value[0])  # Transform the letter into an index
+                col_index = int(input_value[1]) - 1  # Transform human input to index
                 if row_index > BOARD_SIZE - 1 or col_index > BOARD_SIZE - 1:
                     continue
             return row_index, col_index
@@ -46,13 +48,13 @@ class TicTacToe:
         while True:  # Call input until value provided is valid for the board state
             row_index, col_index = self.player_input()
             if self.board[row_index][col_index] == EMPTY_SQUARE:
-                self.board[row_index][col_index] = self.player
+                self.board[row_index][col_index] = self.active_player
                 break
 
     def get_diagonal(self, board):
         """Returns forward diagonal as a list to be added to get_all_lines()"""
         diagonal = []
-        for index in range(len(board)):
+        for index in range(BOARD_SIZE):
             diagonal.append(board[index][index])
         return diagonal
 
@@ -61,9 +63,9 @@ class TicTacToe:
         all_lines = []
         for row in self.board:
             all_lines.append(row)
-        for col_index in range(len(self.board)):
+        for col_index in range(BOARD_SIZE):
             column = []
-            for row_index in range(len(self.board)):
+            for row_index in range(BOARD_SIZE):
                 column.append(self.board[row_index][col_index])
             all_lines.append(column)
         all_lines.append(self.get_diagonal(self.board))
@@ -72,30 +74,28 @@ class TicTacToe:
 
     def win_con(self, lines):
         for line in lines:
-            if line.count(self.player) == 3:
-                print(self.__str__())
-                print(self.player + " wins!")
+            if line.count(self.active_player) == 3:
+                print(self)
+                print(self.active_player + " wins!")
                 return True
 
     def board_full(self):
-        for row in game_board.board:
+        for row in self.board:
             if row.count(EMPTY_SQUARE) != 0:
                 return False
-        print(self.__str__())
+        print(self)
         print("Draw!")
         return True
 
     def game_loop(self):
         while True:
-            print(self.__str__())
+            print(self)
             self.place_piece()
-            if self.win_con(self.get_all_lines()):
-                return True
-            if self.board_full():
-                return True
-            self.player = self.change_player()
+            if self.win_con(self.get_all_lines()) or self.board_full():
+                break
+            self.change_player()
 
 
 if __name__ == "__main__" == __name__:
-    game_board = TicTacToe()
-    game_board.game_loop()
+    TicTacToe()()
+
