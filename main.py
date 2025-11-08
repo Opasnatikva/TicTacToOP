9dc546e16799136b08249602f004b26f72b93826
Todor Vladimirov <damyand@codecommit-at-789734661575>
2025-11-08 15:15:25 +0200

    Refactoring and linting

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+Tic Tac Toe Game
+Author: <Theo>
+Date: 2023-03-26
+License: Unlicensed
+"""
 import string
 
 EMPTY_SQUARE = " "
@@ -7,15 +13,19 @@ PIECE_TWO = "O"
 
 
 class TicTacToe:
+    """A class to represent a Tic Tac Toe game board."""
+
     def __init__(self, size=BOARD_SIZE):
         """Initialise the board, and set the starting player as 'X'."""
         self.board = [[EMPTY_SQUARE for _ in range(size)] for __ in range(size)]
         self.active_player = PIECE_ONE
 
     def __call__(self):
+        """Play the game until a winner is found or the board is full."""
         self.play_game()
 
     def __str__(self):
+        """Return a string representation of the board."""
         output_str = " " * 4
         for index in range(BOARD_SIZE):
             output_str += str(index + 1) + (" " * 3)
@@ -28,23 +38,30 @@ class TicTacToe:
         return output_str
 
     def change_player(self):
+        """Change the active player."""
         self.active_player = PIECE_TWO if self.active_player == PIECE_ONE else PIECE_ONE
 
     def take_player_input(self):
+        """Take player input and return the row and column indices."""
         while True:
-            input_value = input("Please select a row and a column to place the " + self.active_player + "!").upper()
+            input_value = input(
+                "Please select a row and a column to place the " + \
+                self.active_player + "!").upper()
             if len(input_value) != 2:  # Input needs to be 2 characters long
                 continue
-            else:
-                if input_value[0] not in string.ascii_uppercase or not input_value[1].isdigit():  # if value not letter
-                    continue
-                row_index = string.ascii_uppercase.find(input_value[0])  # Transform the letter into an index
-                col_index = int(input_value[1]) - 1  # Transform human input to index
-                if row_index > BOARD_SIZE - 1 or col_index > BOARD_SIZE - 1:
-                    continue
+
+            if input_value[0] not in string.ascii_uppercase or not \
+                    input_value[1].isdigit():  # if value not letter
+                continue
+            row_index = string.ascii_uppercase.find(
+                input_value[0])  # Transform the letter into an index
+            col_index = int(input_value[1]) - 1  # Transform human input to index
+            if row_index > BOARD_SIZE - 1 or col_index > BOARD_SIZE - 1:
+                continue
             return row_index, col_index
 
     def place_piece(self):
+        """Place the active player's piece on the board."""
         while True:  # Call input until value provided is valid for the board state
             row_index, col_index = self.take_player_input()
             if self.board[row_index][col_index] == EMPTY_SQUARE:
@@ -73,13 +90,16 @@ class TicTacToe:
         return all_lines
 
     def check_win_con(self, lines):
+        """Checks if the active player has won the game."""
         for line in lines:
             if line.count(self.active_player) == 3:
                 print(self)
                 print(self.active_player + " wins!")
                 return True
+        return False
 
     def check_board_full(self):
+        """Checks if the board is full."""
         for row in self.board:
             if row.count(EMPTY_SQUARE) != 0:
                 return False
@@ -88,6 +108,7 @@ class TicTacToe:
         return True
 
     def play_game(self):
+        """Play the game until a winner is found or the board is full."""
         while True:
             print(self)
             self.place_piece()
@@ -99,4 +120,3 @@ class TicTacToe:
 if __name__ == "__main__" == __name__:
     game = TicTacToe()
     game()
-
